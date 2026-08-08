30bab3085f8e00970be33d1349db380f59cd963c
Bareau-Lescouezec Camlin <camlin.bareau-lescouezec@etudiant.unc.nc>
2026-08-08 20:09:45 +1100

    Tarifs page

--- src/app/pages/tarifs/tarifs.ts
+++ src/app/pages/tarifs/tarifs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tarifs',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tarifs.html',
   styleUrl: './tarifs.scss',
 })
